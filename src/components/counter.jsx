29e5893753a061f5aa38aabe57b833b8821eaf2d
Albert <ishengomaalbert@gmail.com>
2021-10-26 09:01:39 +0300

    EventHandling & Updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,35 +3,51 @@ import React, { Component } from "react";
 class Counter extends React.Component {
   state = {
     count: 2,
-    tags : ['tag1', 'tag2', 'tag3'],
-    names: ['John', 'James', 'Jackson']
+    tags: ["tag1", "tag2", "tag3"],
+    names: ["John", "James", "Jackson"],
   };
 
   styles = {
     margin: "20px",
   };
+
+  handleIncrement = () => {
+     this.setState({ count: this.state.count + 1 })
+  }
+
   render() {
     return (
       <React.Fragment>
         <span className={this.getSpanClasses()}>{this.formatCount()}</span>
-        <button style={this.styles} className="btn btn-secondary">
+        <button
+          onClick={this.handleIncrement}
+          style={this.styles}
+          className="btn btn-secondary"
+        >
           Increment
         </button>
-          
-          {this.renderTags()}
 
-          <ul>
-             { this.state.names.length === 0 && <p>There re no names!!!</p> }
-             {this.state.names.map(name => <li key={name}>{name}</li>)}
-          </ul>
+        {this.renderTags()}
+
+        <ul>
+          {this.state.names.length === 0 && <p>There re no names!!!</p>}
+          {this.state.names.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
 
-  renderTags(){
-    if(this.state.tags.length === 0 ) return <p>There is no tags!!!</p>;
-     return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags!!!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
   getSpanClasses() {
     let classes = "badge m-2 badge-";
