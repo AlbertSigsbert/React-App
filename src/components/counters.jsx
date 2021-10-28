dcc728142006548929c37005b93e17db16f3c612
Albert <ishengomaalbert@gmail.com>
2021-10-28 09:20:52 +0300

    Composing multiple components

---
+++ src/components/counters.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+class Counters extends React.Component {
+    state = {
+        counters : [
+            {id:1, value: 0},
+            {id:2, value: 1},
+            {id:3, value: 3},
+            {id:4, value: 2},
+        ]
+    }
+    render() { 
+        return <div>
+            { this.state.counters.map(counter => <Counter key={counter.id}/>) }
+        </div>;
+    }
+}
+ 
+export default Counters;
